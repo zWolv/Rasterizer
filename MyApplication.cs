83388da314add3e14df035966d4c392ab293177c
DatDaniel1 <daniel.vanderhoeven2004@gmail.com>
2023-06-12 14:51:51 +0200

    camera

--- MyApplication.cs
+++ MyApplication.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
+using INFOGR2023TemplateP2;
 using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 
 namespace Template
 {
@@ -16,6 +18,7 @@ namespace Template
         RenderTarget? target;                   // intermediate render target
         ScreenQuad? quad;                       // screen filling quad for post processing
         readonly bool useRenderTarget = true;
+        Camera camera;
 
         // constructor
         public MyApplication(Surface screen)
@@ -39,6 +42,8 @@ namespace Template
             // create the render target
             if (useRenderTarget) target = new RenderTarget(screen.width, screen.height);
             quad = new ScreenQuad();
+            camera = new Camera(new Vector3(0, 14.5f, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 0), new Vector3(0, 1, 0));
+            camera.UpdateFrontDirection();
         }
 
         // tick for background surface
@@ -46,6 +51,8 @@ namespace Template
         {
             screen.Clear(0);
             screen.Print("hello world", 2, 2, 0xffff00);
+
+            KeyboardInput(OpenTKApp.keyboard);
         }
 
         // tick for OpenGL rendering code
@@ -60,12 +67,12 @@ namespace Template
             float angle90degrees = MathF.PI / 2;
             Matrix4 Tpot = Matrix4.CreateScale(0.5f) * Matrix4.CreateFromAxisAngle(new Vector3(0, 1, 0), a);
             Matrix4 Tfloor = Matrix4.CreateScale(4.0f) * Matrix4.CreateFromAxisAngle(new Vector3(0, 1, 0), a);
-            Matrix4 Tcamera = Matrix4.CreateTranslation(new Vector3(0, -14.5f, 0)) * Matrix4.CreateFromAxisAngle(new Vector3(1, 0, 0), angle90degrees);
+            Matrix4 Tcamera = Matrix4.LookAt(camera.position, camera.position + camera.frontDirection, camera.upDirection);
             Matrix4 Tview = Matrix4.CreatePerspectiveFieldOfView(1.2f, 1.3f, .1f, 1000);
 
             // update rotation
-            a += 0.001f * frameDuration;
-            if (a > 2 * MathF.PI) a -= 2 * MathF.PI;
+            //a += 0.001f * frameDuration;
+            //if (a > 2 * MathF.PI) a -= 2 * MathF.PI;
 
             if (useRenderTarget && target != null && quad != null)
             {
@@ -92,6 +99,57 @@ namespace Template
                     mesh?.Render(shader, Tpot * Tcamera * Tview, wood);
                     floor?.Render(shader, Tfloor * Tcamera * Tview, wood);
                 }
+            }
+        }
+
+        public void KeyboardInput(KeyboardState keyboard)
+        {
+            if (keyboard[Keys.E])
+                camera.position += camera.upDirection;
+            if (keyboard[Keys.Q])
+                camera.position -= camera.upDirection;
+            if (keyboard[Keys.W])
+                camera.position += camera.frontDirection;
+            if (keyboard[Keys.S])
+                camera.position -= camera.frontDirection;
+            if (keyboard[Keys.A])
+                camera.position += camera.rightDirection;
+            if (keyboard[Keys.D])
+                camera.position -= camera.rightDirection;
+
+            if (keyboard[Keys.Up])
+            {
+                camera.pitch++;
+                if (camera.pitch > 89.0f)
+                {
+                    camera.pitch = 89.0f;
+                }
+                camera.UpdateFrontDirection();
+                camera.UpdateUpDirection();
+            }
+            if (keyboard[Keys.Down])
+            {
+                camera.pitch--;
+                if (camera.pitch < -89.0f)
+                {
+                    camera.pitch = -89.0f;
+                }
+                camera.UpdateFrontDirection();
+                camera.UpdateUpDirection();
+            }
+            if (keyboard[Keys.Left])
+            {
+                camera.yaw++;
+                camera.UpdateFrontDirection();
+                camera.UpdateRightDirection();
+                camera.UpdateUpDirection();
+            }
+            if (keyboard[Keys.Right])
+            {
+                camera.yaw--;
+                camera.UpdateFrontDirection();
+                camera.UpdateRightDirection();
+                camera.UpdateUpDirection();
             }
         }
     }
